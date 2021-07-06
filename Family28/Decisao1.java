f7ccbb3646b4290ee00de6f9aafb80aecd0c599a
Gui Matheus <guilherme.maths.sil@gmail.com>
2021-07-06 07:41:40 -0300

    novas aterações

--- Family28/Decisao1.java
+++ Family28/Decisao1.java
@@ -21,29 +21,25 @@ public class Decisao1 {
 			num3 = observe.nextInt();
 		}
 		
-		if(num1 < num2 && num2 <= num3)
-		{
-			System.out.println("\nMaior numero:" + num1);}
-			
-			else if (num1< num3 && num3 <= num2)
-			{
+		if(num1 > num2 && num1 > num3 )
+		{ 
 			System.out.println("\nMaior numero: " + num1);}
-		
-			else if (num3<num1 && num1<=num2);
-		{
-			System.out.println("\nMaior numero:" + num3);
 			
-			 if (num3< num2 && num2 <= num1) {
-				
-				
+		
+		else if(num2 > num3 && num2 > num1 ) {
+			
+			System.out.println("\nMaior numero: " + num2);
+		}
+		
+		else {
 			System.out.println("\nMaior numero: " + num3);
-			}
-			 else 
-				 System.out.println("\nMaior numero: " + num2);
 		}
-		}
+	}
+			
+			
+	}
+		
 	
 		
-	}
-
+	
 
